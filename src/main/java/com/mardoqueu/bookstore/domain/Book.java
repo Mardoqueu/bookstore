ebf1c2b9b065c8501e0041f87af1aa7777b96526
mardoqueu <mdq767@gmail.com>
2022-10-01 12:39:47 -0300

    finalizing domain classes

--- src/main/java/com/mardoqueu/bookstore/domain/Book.java
+++ src/main/java/com/mardoqueu/bookstore/domain/Book.java
@@ -1,13 +1,29 @@
 package com.mardoqueu.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book implements Serializable{
+
+	private static final long serialVersionUID = 1L;	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String name_author;
 	private String text;
 
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 
 	public Book() {
--- src/main/java/com/mardoqueu/bookstore/domain/Category.java
+++ src/main/java/com/mardoqueu/bookstore/domain/Category.java
@@ -1,14 +1,28 @@
 package com.mardoqueu.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Category {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category implements Serializable{
+
+	private static final long serialVersionUID = 1L;	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description;
 
+	@OneToMany(mappedBy = "category")
 	private List<Book> books = new ArrayList<>();
 
 	public Category() {
