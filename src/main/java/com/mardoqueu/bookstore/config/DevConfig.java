bbc0fd43d7fc7cf5282767059d7391f6accdf350
mardoqueu <mdq767@gmail.com>
2022-10-01 16:06:30 -0300

    DEV profile creation

---
+++ src/main/java/com/mardoqueu/bookstore/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.mardoqueu.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mardoqueu.bookstore.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean databaseInstance() {
+		if(strategy.equals("create")) {
+			this.dbService.databaseInstance();
+		}
+		return false;
+		
+	}
+	
+}
