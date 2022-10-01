6feaab4c96c367c840ef211da3693c435ba39e28
mardoqueu <mdq767@gmail.com>
2022-10-01 12:28:03 -0300

    started domain classes

---
+++ src/main/java/com/mardoqueu/bookstore/domain/Book.java
@@ -0,0 +1,84 @@
+package com.mardoqueu.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+	private Integer id;
+	private String title;
+	private String name_author;
+	private String text;
+
+	private Category category;
+
+	public Book() {
+		super();
+
+	}
+
+	public Book(Integer id, String title, String name_author, String text, Category category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.name_author = name_author;
+		this.text = text;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName_author() {
+		return name_author;
+	}
+
+	public void setName_author(String name_author) {
+		this.name_author = name_author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/com/mardoqueu/bookstore/domain/Category.java
@@ -0,0 +1,78 @@
+package com.mardoqueu.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Category {
+	private Integer id;
+	private String name;
+	private String description;
+
+	private List<Book> books = new ArrayList<>();
+
+	public Category() {
+		super();
+	}
+
+	public Category(Integer id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
