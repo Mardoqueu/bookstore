45cc23b7b3258a42211a029b4ddb71b7ba613761
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-22 11:17:19 -0300

    Adding CorsConfiguration

---
+++ src/main/java/com/mardoqueu/bookstore/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.mardoqueu.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+    	return new WebMvcConfigurer() {
+    		
+    		@Override
+    		public void addCorsMappings(CorsRegistry registry) {
+    	        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+    	        .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    		}
+    	};
+    }
+}
